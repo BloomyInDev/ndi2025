5007881e1f99c70715aa470eb3d721ee3e3622a8
Bloomy <contact@bloomyindev.me>
2025-12-05 00:30:16 +0100

    snake update

--- src/snake/snake.ts
+++ src/snake/snake.ts
@@ -191,26 +191,37 @@ const game = () => {
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowUp":
-      console.log("up");
-      snake.direction.x = 0;
-      snake.direction.y = -1;
+      document.getElementById("up")?.click()
       break;
     case "ArrowDown":
-      console.log("down");
-      snake.direction.x = 0;
-      snake.direction.y = 1;
+      document.getElementById("down")?.click()
       break;
     case "ArrowLeft":
-      console.log("left");
-      snake.direction.x = -1;
-      snake.direction.y = 0;
+      document.getElementById("left")?.click()
+      break;
       break;
     case "ArrowRight":
-      console.log("right");
-      snake.direction.x = 1;
-      snake.direction.y = 0;
+      document.getElementById("right")?.click()
+      break;
       break;
   }
+});
+
+document.getElementById("up")?.addEventListener("click", () => {
+  snake.direction.x = 0;
+  snake.direction.y = -1;
+});
+document.getElementById("down")?.addEventListener("click", () => {
+  snake.direction.x = 0;
+  snake.direction.y = 1;
+});
+document.getElementById("left")?.addEventListener("click", () => {
+  snake.direction.x = -1;
+  snake.direction.y = 0;
+});
+document.getElementById("right")?.addEventListener("click", () => {
+  snake.direction.x = 1;
+  snake.direction.y = 0;
 });
 
 resize();
