14c8f73cf3ad5ffc48accd882b83e5d11bfdc6f3
Bloomy <contact@bloomyindev.me>
2025-12-05 02:09:34 +0100

    fix: update paths in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url)
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
@@ -15,7 +15,16 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, "src/index.html"),
-                snake: resolve(__dirname, "src/snake/index.html"),
+                biblioteque: resolve(__dirname, "src/biblioteque.html"),
+                centre_technique: resolve(
+                    __dirname,
+                    "src/centre_technique.html"
+                ),
+                communication: resolve(__dirname, "src/communication.html"),
+                ecole: resolve(__dirname, "src/ecole.html"),
+                mairie: resolve(__dirname, "src/mairie.html"),
+                maison: resolve(__dirname, "src/maison.html"),
+                snake: resolve(__dirname, "src/snake.html"),
             },
         },
     },
